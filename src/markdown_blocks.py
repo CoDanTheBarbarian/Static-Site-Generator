6236fafcc4bd8fcc32c294bb35af67302df6fb72
CoDanTheBarbarian <plafcandaniel@gmail.com>
2024-09-05 14:58:09 -0700

    18: Added a block_to_block_type type identifier function and a test suite for various cases.

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,5 @@
+import re
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -6,3 +8,28 @@ def markdown_to_blocks(markdown):
             continue
         filtered_blocks.append(block.strip())
     return filtered_blocks
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block):
+        return "heading"
+    lines = block.split("\n")
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
+        return "code"
+    quote = True
+    unordered_list = True
+    for line in lines:
+        if not line.startswith(">"):
+            quote = False
+        if not line.startswith("* ") and not line.startswith("- "):
+            unordered_list = False
+    if quote:
+        return "quote"
+    if unordered_list:
+        return "unordered_list"
+    ordered_list = True
+    for i, line in enumerate(lines):
+        if not line.startswith(f"{(i + 1)}. "):
+            ordered_list = False
+    if ordered_list:
+        return "ordered_list"
+    return "paragraph"
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,24 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+def test_block_to_block_type(self):
+    test_cases = [
+        ("### This is a heading", "heading"),
+        ("```This is code```", "code"),
+        ("```This is two\nlines of code```", "code"),
+        ("> This is a\n>multiple line\n>quote", "quote"),
+        ("* This is an unordered list\n- with different markdown symbols", "unordered_list"),
+        ("1. This is an ordered list\n2. That satisfies the conditions\n3. With three items", "ordered_list"),
+        ("####### This is a non-heading (7 pound signs)\nthat should return 'paragraph'", "paragraph"),
+        ("# Heading 1", "heading"),
+        ("###### Heading 6", "heading"),
+        ("1. Not an ordered list\n3. Because it doesn't increment properly", "paragraph")
+    ]
+    
+    for block, expected_type in test_cases:
+        with self.subTest(block=block):
+            self.assertEqual(block_to_block_type(block), expected_type)
 
 
 if __name__ == "__main__":
